b1f9edded47821559427f63a9c1b43e38dcdad0a
raAstIN <therealseyed@gmail.com>
2025-09-18 01:44:10 +0330

    Update script.js

--- en/assets/js/script.js
+++ en/assets/js/script.js
@@ -114,15 +114,16 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let k = 0; k < navigationLinks.length; k++) {
-      if (navigationLinks[k].classList.contains("active")) {
-        navigationLinks[k].classList.remove("active");
-        pages[k].classList.remove("active");
-      }
+    for (let j = 0; j < pages.length; j++) {
+      pages[j].classList.remove("active");
+      navigationLinks[j].classList.remove("active");
     }
 
     this.classList.add("active");
-    pages[i].classList.add("active");
+    const targetPage = document.querySelector(`[data-page="${this.innerHTML.toLowerCase()}"]`);
+    if (targetPage) {
+      targetPage.classList.add("active");
+    }
     window.scrollTo(0, 0);
   });
 }
